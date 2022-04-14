ca6fda564709936abf3db8a455886b42aaffe18c
TatianaGuzun <tatiana.guzun@reachplc.com>
2022-04-14 16:27:48 +0100

    innitial commit

---
+++ products/models.py
@@ -0,0 +1,24 @@
+from multiprocessing import Condition
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    brand = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    image = models.CharField(max_length=300)
+    price = models.PositiveSmallIntegerField()
+    category = models.ForeignKey(
+        Category, related_name='products', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} {self.brand} from {self.category} category'
