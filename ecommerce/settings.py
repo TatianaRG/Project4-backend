b8c458c1e5c01b2cf83e48f83720f4965b5fffad
TatianaGuzun <tatiana.guzun@reachplc.com>
2022-04-14 18:16:42 +0100

    fixed error forbiden 403

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -156,6 +156,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ['https://mynewbookstore.herokuapp.com']
+CSRF_TRUSTED_ORIGINS = ['https://rewearapp.herokuapp.com']
 
 django_on_heroku.settings(locals())
